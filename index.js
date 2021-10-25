2b193bb8a9c0532ec2632a5744b58343864026fd
Tahmid Islam <tahmid231@gmail.com>
2021-10-25 19:52:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Ema John Server Is Running");
+});
+
+app.listen(port, () => {
+  console.log("Server running at port", port);
+});
